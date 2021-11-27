40704eb7d6eff08031b61c4e3b7c9c18b7a60531
karime-lomeli <75227467+karime-lomeli@users.noreply.github.com>
2021-11-27 14:41:48 -0600

    having

--- controllers/controller_getMaximo.php
+++ controllers/controller_getMaximo.php
@@ -1,3 +1,20 @@
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 90%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
 <?php
 include_once '../connection/Object_Connection.php';
 include_once '../models/Object_maximo.php';
@@ -12,9 +29,30 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     ?>
 
 <h6>Nombre del articulo: <?php echo $row['nombre']; ?></h6>
-<h6>Inversion: <?php echo $row['mul']; ?></h6><br>
-<?php
+<h6>Inversion: <?php echo $row['mul']; ?></h6><br><?php
 }
 
+$resurtir= new Maximo($db);
+$aux = $resurtir->getResurtir();
+?><h5>A punto de agotarse en bodega.</h5><br>
+<table>
+<tr>
+    <th>Nombre del Articulo</th>
+    <th>Existencia</th>
+    <th>Proveedor</th>
+    <th>Telefono</th>
+  </tr>
+<?php
+while ($aux2 = $aux->fetch(PDO::FETCH_ASSOC)) {
+    ?>
+    <tr>
+    <td> <?php echo $aux2['nombre']; ?></td>
+    <td> <?php echo $aux2['existencia']; ?></td>
+    <td> <?php echo $aux2['nomprov']; ?></td>
+    <td> <?php echo $aux2['telefono']; ?></td>
+    </tr>
 
+<?php
+} 
 ?>
+</table>
--- models/Object_Maximo.php
+++ models/Object_Maximo.php
@@ -14,15 +14,23 @@ class Maximo {
         $query = "SELECT a.nombre,max((b.existencia*b.precio))as mul FROM u672703426_cutsiegirl.articulo a, u672703426_cutsiegirl.inventario b where a.idArticulo=b.idArticulo";
 
         $stmt = $this->conn->prepare($query);
-        
-       
-       
-
         if($stmt->execute())
             return $stmt;
         else
             return 0; 
 
     }    
+
+    function getResurtir() {
+        
+        $query = "SELECT a.nombre,b.existencia, (c.nombre)as nomprov, c.telefono FROM u672703426_cutsiegirl.articulo a, u672703426_cutsiegirl.inventario b,u672703426_cutsiegirl.proveedores c where a.idArticulo=b.idArticulo and a.idProveedor=c.idProveedor having b.existencia<4";
+
+        $stmt = $this->conn->prepare($query);
+        if($stmt->execute())
+            return $stmt;
+        else
+            return 0; 
+
+    }   
 }
 ?>
